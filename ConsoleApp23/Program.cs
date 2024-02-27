8c9cfa1e296250ba4ba1c2b2e70e133fe1bc4e6b
Victor R. Silva <victorr.silva005@gmail.com>
2024-02-27 07:24:03 -0300

    Transformed CalculationService into a generic class

--- ConsoleApp23/Program.cs
+++ ConsoleApp23/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApp23.Entities;
+using ConsoleApp23.Services;
 namespace ConsoleApp23
 {
     internal class Program
@@ -8,12 +9,19 @@ namespace ConsoleApp23
             Console.Write("Enter N: ");
             int n = int.Parse(Console.ReadLine());
 
-            Resources resources = new Resources();
+            List<int> list = new List<int>();
 
             for (int i = 0; i < n; i++)
             {
-
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
             }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+
+            Console.WriteLine("Max: " + max);
         }
     }
 }
